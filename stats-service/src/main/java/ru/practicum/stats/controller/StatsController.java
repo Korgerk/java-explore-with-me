ed8943d29bad484155678a90fedb4202a9a2a402
Nohum <savva.gubarev228@mail.ru>
2025-12-07 18:40:46 +0300

    1

--- stats-service/src/main/java/ru/practicum/stats/controller/StatsController.java
+++ stats-service/src/main/java/ru/practicum/stats/controller/StatsController.java
@@ -1,14 +1,13 @@
 package ru.practicum.stats.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stats.Hit;
 import ru.practicum.stats.ViewStats;
 import ru.practicum.stats.service.StatsService;
 
+import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,13 +18,16 @@ public class StatsController {
     private final StatsService statsService;
 
     @PostMapping("/hit")
-    @ResponseStatus(HttpStatus.CREATED)
     public void addHit(@RequestBody @Valid Hit hit) {
         statsService.save(hit);
     }
 
     @GetMapping("/stats")
-    public List<ViewStats> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end, @RequestParam(required = false) List<String> uris, @RequestParam(defaultValue = "false") boolean unique) {
+    public List<ViewStats> getStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") boolean unique) {
 
         return statsService.getStats(start, end, uris, unique);
     }
