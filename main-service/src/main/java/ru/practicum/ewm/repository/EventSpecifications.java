6f96a7e54e86d86422b3c160439272550e0fca3a
Nohum <savva.gubarev228@mail.ru>
2025-12-15 12:17:30 +0300

    Update EventSpecifications.java

--- main-service/src/main/java/ru/practicum/ewm/repository/EventSpecifications.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/EventSpecifications.java
@@ -1,8 +1,6 @@
 package ru.practicum.ewm.repository;
 
 import jakarta.persistence.criteria.Expression;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import org.springframework.data.jpa.domain.Specification;
 import ru.practicum.ewm.model.Event;
 import ru.practicum.ewm.model.enums.EventState;
@@ -10,34 +8,33 @@ import ru.practicum.ewm.model.enums.EventState;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@FieldDefaults(level = AccessLevel.PUBLIC)
 public class EventSpecifications {
 
-    static Specification<Event> hasState(EventState state) {
+    public static Specification<Event> hasState(EventState state) {
         return (root, query, cb) -> cb.equal(root.get("state"), state);
     }
 
-    static Specification<Event> initiatorIn(List<Long> userIds) {
+    public static Specification<Event> initiatorIn(List<Long> userIds) {
         return (root, query, cb) -> root.get("initiator").get("id").in(userIds);
     }
 
-    static Specification<Event> categoryIn(List<Long> categoryIds) {
+    public static Specification<Event> categoryIn(List<Long> categoryIds) {
         return (root, query, cb) -> root.get("category").get("id").in(categoryIds);
     }
 
-    static Specification<Event> stateIn(List<EventState> states) {
+    public static Specification<Event> stateIn(List<EventState> states) {
         return (root, query, cb) -> root.get("state").in(states);
     }
 
-    static Specification<Event> dateAfter(LocalDateTime start) {
+    public static Specification<Event> dateAfter(LocalDateTime start) {
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("eventDate"), start);
     }
 
-    static Specification<Event> dateBefore(LocalDateTime end) {
+    public static Specification<Event> dateBefore(LocalDateTime end) {
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("eventDate"), end);
     }
 
-    static Specification<Event> textLike(String text) {
+    public static Specification<Event> textLike(String text) {
         return (root, query, cb) -> {
             String like = "%" + text.toLowerCase() + "%";
             Expression<String> ann = cb.lower(root.get("annotation"));
@@ -46,7 +43,7 @@ public class EventSpecifications {
         };
     }
 
-    static Specification<Event> paidIs(Boolean paid) {
+    public static Specification<Event> paidIs(Boolean paid) {
         return (root, query, cb) -> cb.equal(root.get("paid"), paid);
     }
 }
